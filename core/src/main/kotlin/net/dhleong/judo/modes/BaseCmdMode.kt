cd422720ae64c488e2e8b97b1877d8a6bed1ab5c
Daniel Leong <me@dhleong.net>
2017-06-08 08:17:05 -0400

    Add some super basic help files (in prep for cmd tab completion)

---
+++ core/src/main/kotlin/net/dhleong/judo/modes/BaseCmdMode.kt
@@ -0,0 +1,224 @@
+package net.dhleong.judo.modes
+
+import net.dhleong.judo.IJudoCore
+import net.dhleong.judo.Mode
+import net.dhleong.judo.complete.CompletionSuggester
+import net.dhleong.judo.complete.DumbCompletionSource
+import net.dhleong.judo.input.IInputHistory
+import net.dhleong.judo.input.InputBuffer
+import net.dhleong.judo.util.hasCtrl
+import java.awt.event.KeyEvent
+import java.io.InputStream
+import javax.swing.KeyStroke
+
+/**
+ * @author dhleong
+ */
+
+private fun buildHelp(usage: String, description: String): String =
+    buildHelp(listOf(usage), description)
+private fun buildHelp(usages: Iterable<String>, description: String): String =
+    with(StringBuilder()) {
+        var maxUsageLength = 0
+        usages.forEach {
+            appendln(it)
+            maxUsageLength = maxOf(maxUsageLength, it.length)
+        }
+        appendln("=".repeat(maxUsageLength))
+        appendln(description)
+
+        toString()
+    }
+
+private val COMMAND_HELP = mutableMapOf(
+    "alias" to buildHelp(
+        listOf(
+            "alias(inputSpec: String, outputSpec: String)",
+            "alias(inputSpec: String, handler: Fn)",
+            "@alias(inputSpec: String)"
+        ),
+        "Create a text alias."
+    ),
+
+    "prompt" to buildHelp(
+        listOf(
+            "prompt(inputSpec: String, outputSpec: String)",
+            "prompt(inputSpec: String, handler: Fn)",
+            "@prompt(inputSpec: String)"
+        ),
+        "Prepare a prompt to be displayed in the status area."
+    ),
+
+    "trigger" to buildHelp(
+        listOf(
+            "trigger(inputSpec: String, handler: Fn)",
+            "@trigger(inputSpec: String)"
+        ),
+        "Declare a trigger."
+    ),
+
+    "connect" to buildHelp(
+        "connect(host: String, port: Int)",
+        "Connect to a server."
+    ),
+
+    "disconnect" to buildHelp(
+        "disconnect()",
+        "Disconnect from the server."
+    ),
+
+    "echo" to buildHelp(
+        "echo(...)",
+        "Print some output to the screen locally."
+    ),
+
+    "quit" to buildHelp(
+        "quit()",
+        "Exit Judo"
+    ),
+
+    "send" to buildHelp(
+        "send(text: String)",
+        "Send some text to the connected server."
+    ),
+
+    "startInsert" to buildHelp(
+        "startInsert()",
+        "Enter insert mode as if by pressing `i`"
+    ),
+
+    "stopInsert" to buildHelp(
+        "stopInsert()",
+        "Exit insert mode as soon as possible"
+    )
+
+).apply {
+    val mapTypes = arrayOf(
+        "map", "noremap",
+        "imap", "inoremap",
+        "nmap", "nnoremap")
+    val help = buildHelp(
+        mapTypes.map {
+            "$it(inputKeys: String, outputKeys: String)"
+        },
+        "Create a mapping in a specific mode from inputKeys to outputKeys"
+    )
+    mapTypes.forEach {
+        put(it, help)
+    }
+}
+
+abstract class BaseCmdMode(
+    val judo: IJudoCore,
+    val inputBuffer: InputBuffer,
+    val history: IInputHistory
+) : Mode {
+
+    override val name = "cmd"
+
+    private val suggester = CompletionSuggester(DumbCompletionSource().apply {
+        COMMAND_HELP.keys.forEach(this::process)
+    })
+
+    override fun feedKey(key: KeyStroke, remap: Boolean) {
+        when {
+            key.keyCode == KeyEvent.VK_ENTER -> {
+                val code = inputBuffer.toString().trim()
+                when (code) {
+                    "q", "q!", "qa", "qa!" -> {
+                        judo.quit()
+                        return
+                    }
+                }
+
+                if (code == "help") {
+                    showHelp()
+                } else if (code.startsWith("help")) {
+                    showHelp(code.substring(5))
+                } else {
+                    execute(code)
+                    history.push(code)
+                }
+                
+                clearBuffer()
+                exitMode()
+                return
+            }
+
+            key.keyChar == 'a' && key.hasCtrl() -> {
+                inputBuffer.cursor = 0
+                return
+            }
+
+            key.keyChar == 'c' && key.hasCtrl() -> {
+                clearBuffer()
+                exitMode()
+                return
+            }
+
+            key.keyChar == 'e' && key.hasCtrl() -> {
+                inputBuffer.cursor = inputBuffer.size
+                return
+            }
+
+            key.keyCode == KeyEvent.VK_UP -> {
+                history.scroll(-1)
+                return
+            }
+
+            key.keyCode == KeyEvent.VK_DOWN -> {
+                history.scroll(1)
+                return
+            }
+        }
+
+        if (key.hasCtrl()) {
+            // ignore
+            return
+        }
+
+        insertChar(key)
+    }
+
+    private fun showHelp() {
+        // TODO columns?
+        COMMAND_HELP.keys.forEach { judo.echo(it) }
+    }
+
+    private fun showHelp(command: String) {
+        COMMAND_HELP[command]?.let {
+            it.split("\n").forEach { judo.echo(it) }
+            return
+        }
+
+        judo.echo("No such command: $command")
+    }
+
+
+    private fun exitMode() {
+        judo.exitMode()
+    }
+
+    /**
+     * Insert a key stroke at the current cursor position
+     */
+    private fun insertChar(key: KeyStroke) {
+        val wasEmpty = inputBuffer.isEmpty()
+        inputBuffer.type(key)
+        if (inputBuffer.isEmpty() && wasEmpty) {
+            exitMode()
+        }
+    }
+
+    override fun onEnter() {
+        clearBuffer()
+    }
+
+    abstract fun execute(code: String)
+
+    abstract fun readFile(fileName: String, stream: InputStream)
+
+    private fun clearBuffer() {
+        inputBuffer.clear()
+    }
+}
