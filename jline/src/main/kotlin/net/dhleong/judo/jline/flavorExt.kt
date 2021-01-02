897269b263a7c8eebb9c22e55eab89195b2ed887
Daniel Leong <me@dhleong.net>
2021-01-02 16:14:36 -0500

    WIP: Upgrade JLine to 3.18
    Fixes #113
    
    There are several bugs with unreleased fixes that we're waiting on...

--- jline/src/main/kotlin/net/dhleong/judo/jline/flavorExt.kt
+++ jline/src/main/kotlin/net/dhleong/judo/jline/flavorExt.kt
@@ -9,22 +9,22 @@ import org.jline.utils.AttributedStyle
 import kotlin.math.round
 
 // from AttributedStyle source, since they're not public...
-private const val F_BOLD = 0x00000001
-private const val F_FAINT = 0x00000002
-private const val F_ITALIC = 0x00000004
-private const val F_UNDERLINE = 0x00000008
-private const val F_BLINK = 0x00000010
-private const val F_INVERSE = 0x00000020
-private const val F_CONCEAL = 0x00000040
-private const val F_CROSSED_OUT = 0x00000080
-private const val F_FOREGROUND = 0x00000100
-private const val F_BACKGROUND = 0x00000200
-private const val F_HIDDEN = 0x00000400
+private const val F_BOLD = 0x00000001L
+private const val F_FAINT = 0x00000002L
+private const val F_ITALIC = 0x00000004L
+private const val F_UNDERLINE = 0x00000008L
+private const val F_BLINK = 0x00000010L
+private const val F_INVERSE = 0x00000020L
+private const val F_CONCEAL = 0x00000040L
+private const val F_CROSSED_OUT = 0x00000080L
+private const val F_FOREGROUND = 0x00000100L
+private const val F_BACKGROUND = 0x00000400L
+private const val F_HIDDEN = 0x00001000L
 
 fun Flavor.toAttributedStyle(
     colorTheme: ColorTheme? = null
 ): AttributedStyle = createAttributedStyle(
-    0 or isBold.toFlag(F_BOLD)
+    0L or isBold.toFlag(F_BOLD)
         or isFaint.toFlag(F_FAINT)
         or isItalic.toFlag(F_ITALIC)
         or isUnderline.toFlag(F_UNDERLINE)
@@ -32,8 +32,8 @@ fun Flavor.toAttributedStyle(
         or isInverse.toFlag(F_INVERSE)
         or isConceal.toFlag(F_CONCEAL)
         or isStrikeThrough.toFlag(F_CROSSED_OUT)
-        or hasForeground.toFlag(F_FOREGROUND)
-        or hasBackground.toFlag(F_BACKGROUND)
+//        or hasForeground.toFlag(F_FOREGROUND)
+//        or hasBackground.toFlag(F_BACKGROUND)
         or isHidden.toFlag(F_HIDDEN)
 ).let { base ->
 
@@ -41,45 +41,33 @@ fun Flavor.toAttributedStyle(
     var style = base
     if (hasForeground) {
         val fg = colorTheme.transformForeground(foreground)
-        style = if (fg is JudoColor.Default) {
-            style.foregroundDefault()
-        } else {
-            style.foreground(fg.toAnsiInt())
+        style = when (fg) {
+            is JudoColor.Default -> style.foregroundDefault()
+            is JudoColor.FullRGB -> style.foreground(fg.red, fg.green, fg.blue)
+            else -> style.foreground(fg.toAnsiInt())
         }
     }
     if (hasBackground) {
         val bg = colorTheme.transformBackground(background)
-        style = if (bg is JudoColor.Default) {
-            style.backgroundDefault()
-        } else {
-            style.background(bg.toAnsiInt())
+        style = when (bg) {
+            is JudoColor.Default -> style.backgroundDefault()
+            is JudoColor.FullRGB -> style.background(bg.red, bg.green, bg.blue)
+            else -> style.background(bg.toAnsiInt())
         }
     }
 
     style
 }
 
-private val styleFactory by lazy(LazyThreadSafetyMode.NONE) {
-    val constructor = AttributedStyle::class.java.getDeclaredConstructor(
-        Int::class.java,
-        Int::class.java
-    ).apply {
-        isAccessible = true
-    }
-    return@lazy { style: Int, mask: Int ->
-        constructor.newInstance(style, mask)
-    }
-}
-
-fun createAttributedStyle(styleInt: Int): AttributedStyle = styleFactory(
-    styleInt,
-    styleInt
+fun createAttributedStyle(styleLong: Long): AttributedStyle = AttributedStyle(
+    styleLong,
+    styleLong
 )
 
 @Suppress("NOTHING_TO_INLINE")
-private inline fun Boolean.toFlag(flagValue: Int): Int =
+private inline fun Boolean.toFlag(flagValue: Long): Long =
     if (this) flagValue
-    else 0
+    else 0L
 
 internal fun JudoColor.toAnsiInt(): Int = when (this) {
     is JudoColor.Simple -> value.ansi
